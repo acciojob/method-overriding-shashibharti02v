315b13978e8089bae7c3711866f5b21be0e1f88a
Shashi Bharti <shashibharti02v@gmail.com>
2023-07-12 10:59:17 +0530

    Add assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,14 @@ package com.driver;
     class A
     {
         public String meth(){
+
             return "Invoking method from class A";
     }
     }
     class B extends A
     {
         @Override
-        public String meth()
-        {
+        public String meth(){
             return "Method is overridden in extended class B";
         }
     }
